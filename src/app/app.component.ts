4d0ce9f77b700ca40573fd1950c4110aefbd5433
Eunice Kanja <kanjawambui30@gmail.com>
2022-03-16 21:02:33 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cart';
+  title = 'E-commerce cart';
+  mainImage=document.querySelector('.main-pic img')
   imagePaths:string[]=[
+    '/assets/images/image-product-1-thumbnail.jpg',
+    '/assets/images/image-product-2-thumbnail.jpg',
+    '/assets/images/image-product-3-thumbnail.jpg',
+    '/assets/images/image-product-4-thumbnail.jpg',
+
+  ]
+  images:string[]=[
     '/assets/images/image-product-1.jpg',
     '/assets/images/image-product-2.jpg',
     '/assets/images/image-product-3.jpg',
     '/assets/images/image-product-4.jpg',
-
   ]
   constructor(){}
+  next(){
+    
+  }
+  prev(){}
 }
